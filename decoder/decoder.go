f217b5013b3663bdf4ca3c860546657dd2f673e8
sebasion9 <github@franzlla.ng>
2026-01-25 04:55:00 +0100

    abstract with parser struct add parsing for DQT, DHT tables into structs

--- decoder/decoder.go
+++ decoder/decoder.go
@@ -1,42 +1,43 @@
 package decoder
 
 import (
-	"encoding/binary"
 	"fmt"
 	"gstroke/errors"
 )
 
-type validSeg uint16
-var (
-	SOI validSeg = 0xFFD8
-	DQT validSeg = 0xFFDB
-	DHT validSeg = 0xFFC4
-	SOF validSeg = 0xFFC0
-	SOS validSeg = 0xFFDA
-	EOI validSeg = 0xFFD9
+type QuantTable struct {
+	ID uint8 //  Tq
+	Precision uint8 // Pq -> 8 or 16bit
+	values [64]uint16
+}
+
+type huffClass uint8
+const (
+	DC huffClass = 0
+	AC huffClass = 1
 )
 
+type HuffTable struct {
+	Class huffClass
+	ID uint8
+	Counts [16]uint8
+	Symbols []uint8
+}
+
 type Decoder struct {
-	source []byte
-	pos int
-	endPos int
-	dqt [][]byte
-	dht [][]byte
-	sof []byte
-	sos []byte
-	scan []byte
+	*Parser
+	dqt []QuantTable
+	dht []HuffTable
 }
 
 func NewDecoder(source []byte) *Decoder {
 	return &Decoder{
-		source: source, 
-		pos: 0,
+		Parser: newParser(source),
 	}
 }
 
-
 func (d* Decoder) Decode() error {
-	fmt.Println("[INFO] start decoding (delete later)")
+	fmt.Println("[INFO] start parsing (delete later)")
 
 	pos := d.searchSeg(SOI)
 	if pos == -1 {
@@ -57,7 +58,12 @@ func (d* Decoder) Decode() error {
 		}
 		d.pos = pos
 
-		if err := d.readDQT(); err != nil { return err }
+		tables, err := d.parseDQT()
+		if err != nil {
+			return err
+		}
+
+		d.dqt = append(d.dqt, tables...)
 
 	}
 
@@ -70,10 +76,15 @@ func (d* Decoder) Decode() error {
 		}
 		d.pos = pos
 
-		if err := d.readDHT(); err != nil { return err }
+
+		tables, err := d.parseDHT()
+		if err != nil {
+			return err
+		}
+
+		d.dht = append(d.dht, tables...)
 
 	}
-
 	// restart positon to 0
 	d.pos = 0
 
@@ -82,7 +93,8 @@ func (d* Decoder) Decode() error {
 		return errors.NewInvalidJPEGError("No SOF marker")
 	}
 
-	if err := d.readSOF(); err != nil { return err }
+	//TODO:
+	if err := d.parseSOF(); err != nil { return err }
 
 	d.pos = d.searchSeg(SOS)
 	if d.pos == -1 {
@@ -90,105 +102,9 @@ func (d* Decoder) Decode() error {
 	}
 
 
-	if err := d.readSOS(); err != nil { return err }
-	if err := d.readScan(); err != nil { return err }
+	//TODO:
+	if err := d.parseSOS(); err != nil { return err }
+	if err := d.parseScan(); err != nil { return err }
 
-	return nil
-}
-
-func (d* Decoder) searchSeg(seg validSeg) int {
-	srcLen := len(d.source)
-	i := d.pos
-	for ; i < srcLen - 1; i++ {
-		marker := uint16(d.source[i]) << 8 | uint16(d.source[i+1])
-		if validSeg(marker) == seg {
-			return i
-		}
-
-	}
-
-	return -1
-}
-
-// define quantisation table, len 69 bytes
-func (d* Decoder) readDQT() error {
-	// advance marker bytes
-	pos := d.pos + 2
-	if pos + 2 > len(d.source) {
-		return errors.NewInvalidJPEGError("Invalid DQT segment")
-	}
-
-	segSize := int(binary.BigEndian.Uint16(d.source[pos:pos+2]))
-
-
-	table := make([]byte, segSize-2)
-	copy(table, d.source[pos+2:pos+segSize])
-
-	d.dqt = append(d.dqt, table)
-	d.pos = pos+segSize
-
-	return nil
-}
-
-// define huffman table
-func (d* Decoder) readDHT() error {
-	// advance marker bytes
-	pos := d.pos + 2
-	if pos + 2 > len(d.source) {
-		return errors.NewInvalidJPEGError("Invalid DHT segment")
-	}
-
-	segSize := int(binary.BigEndian.Uint16(d.source[pos:pos+2]))
-
-	table := make([]byte, segSize-2)
-	copy(table, d.source[pos+2:pos+segSize])
-
-	d.dht = append(d.dht, table)
-	d.pos = pos+segSize
-
-	return nil
-}
-
-// start of frame, entropy-coded baseline frame
-func(d* Decoder) readSOF() error {
-	// advance marker bytes
-	pos := d.pos + 2
-	if pos + 2 > len(d.source) {
-		return errors.NewInvalidJPEGError("Invalid SOF segment")
-	}
-
-	segSize := int(binary.BigEndian.Uint16(d.source[pos:pos+2]))
-
-	table := make([]byte, segSize - 2)
-	copy(table, d.source[pos+2:pos+segSize])
-	d.sof = table
-
-	d.pos = pos+segSize
-	return nil
-}
-
-// start of scan
-func(d* Decoder) readSOS() error {
-	// advance marker bytes
-	pos := d.pos + 2
-	if pos + 2 > len(d.source) {
-		return errors.NewInvalidJPEGError("Invalid SOF segment")
-	}
-
-	segSize := int(binary.BigEndian.Uint16(d.source[pos:pos+2]))
-
-	table := make([]byte, segSize - 2)
-	copy(table, d.source[pos+2:pos+segSize])
-	d.sos = table
-
-	d.pos = pos+segSize
-	return nil
-}
-
-func(d *Decoder) readScan() error {
-	table := make([]byte, d.endPos - d.pos)
-	copy(table, d.source[d.pos:d.endPos])
-	d.scan = table
-	d.pos = d.endPos
 	return nil
 }
---
+++ decoder/parser.go
@@ -0,0 +1,213 @@
+package decoder
+
+import (
+	"encoding/binary"
+	"gstroke/errors"
+)
+
+type validSeg uint16
+var (
+	SOI validSeg = 0xFFD8
+	DQT validSeg = 0xFFDB
+	DHT validSeg = 0xFFC4
+	SOF validSeg = 0xFFC0
+	SOS validSeg = 0xFFDA
+	EOI validSeg = 0xFFD9
+)
+
+type Parser struct {
+	source []byte
+	pos int
+	endPos int
+	// dqt [][]byte
+	dht [][]byte
+	sof []byte
+	sos []byte
+	scan []byte
+}
+
+func newParser(source []byte) *Parser {
+	return &Parser{
+		source: source, 
+	}
+}
+
+
+func (p* Parser) searchSeg(seg validSeg) int {
+	srcLen := len(p.source)
+	i := p.pos
+	for ; i < srcLen - 1; i++ {
+		marker := uint16(p.source[i]) << 8 | uint16(p.source[i+1])
+		if validSeg(marker) == seg {
+			return i
+		}
+
+	}
+
+	return -1
+}
+
+// define quantisation table, len 69 bytes
+func (p* Parser) parseDQT() ([]QuantTable, error) {
+	var tables []QuantTable
+
+	// advance marker bytes
+	pos := p.pos + 2
+	if pos + 2 > len(p.source) {
+		return nil, errors.NewInvalidJPEGError("Invalid DQT segment")
+	}
+
+	segSize := int(binary.BigEndian.Uint16(p.source[pos:pos+2]))
+	// advance seg size
+	pos += 2
+	// end of segment
+	end := pos + (segSize - 2)
+
+
+	for ; pos < end; {
+		header := p.source[pos]
+		precision := header >> 4
+		id := header & 0x0F
+
+		// advance dqt header
+		pos++
+
+		bytesPerVal := 1
+		if precision == 1 {
+			bytesPerVal = 2
+		} else if precision != 0 {
+			return nil, errors.NewInvalidJPEGError("Invalid DQT precision")
+		}
+
+		size := 64 * bytesPerVal
+
+		if pos + size > end {
+			return nil, errors.NewInvalidJPEGError("Truncated DQT table")
+		}
+
+		data := p.source[pos:pos+size]
+		pos += size
+
+		var values [64]uint16
+		for i := range 64 {
+			if precision == 0 {
+				values[i] = uint16(data[i])
+				continue
+			}
+			values[i] = binary.BigEndian.Uint16(data[i*2:i*2+2])
+		}
+
+		tables = append(tables, QuantTable{
+			Precision: precision,
+			ID: id,
+			values: values,
+		})
+
+	}
+
+
+	p.pos = end
+
+
+	return tables, nil
+}
+
+// define huffman table
+func (p* Parser) parseDHT() ([]HuffTable, error) {
+	var tables []HuffTable
+	// advance marker bytes
+	pos := p.pos + 2
+	if pos + 2 > len(p.source) {
+		return nil, errors.NewInvalidJPEGError("Invalid DHT segment")
+	}
+
+	segSize := int(binary.BigEndian.Uint16(p.source[pos:pos+2]))
+	pos += 2
+	end := pos + segSize - 2
+
+	for ; pos < end; {
+		header := p.source[pos]
+		pos++
+
+		class := header >> 4
+		id := header & 0x0F
+		if class > 1 {
+			return nil, errors.NewInvalidJPEGError("Invalid DHT class")
+		}
+
+		var counts [16]uint8
+		copy(counts[:], p.source[pos:pos+16])
+		pos+=16
+
+		symbolsNum := 0
+		for _, c := range counts {
+			symbolsNum += int(c)
+		}
+
+		if pos + symbolsNum > end {
+			return nil, errors.NewInvalidJPEGError("Trucated DHT symbols")
+		}
+
+		symbols := make([]uint8, symbolsNum)
+		copy(symbols, p.source[pos:pos+symbolsNum])
+		pos += symbolsNum
+
+
+		tables = append(tables, HuffTable{
+			Class: huffClass(class),
+			ID: id,
+			Counts: counts,
+			Symbols: symbols,
+		})
+
+	}
+
+
+	p.pos = end
+
+	return tables, nil
+}
+
+// start of frame, entropy-coded baseline frame
+func(p Parser) parseSOF() error {
+	// advance marker bytes
+	pos := p.pos + 2
+	if pos + 2 > len(p.source) {
+		return errors.NewInvalidJPEGError("Invalid SOF segment")
+	}
+
+	segSize := int(binary.BigEndian.Uint16(p.source[pos:pos+2]))
+
+	table := make([]byte, segSize - 2)
+	copy(table, p.source[pos+2:pos+segSize])
+	p.sof = table
+
+	p.pos = pos+segSize
+	return nil
+}
+
+// start of scan
+func(p Parser) parseSOS() error {
+	// advance marker bytes
+	pos := p.pos + 2
+	if pos + 2 > len(p.source) {
+		return errors.NewInvalidJPEGError("Invalid SOF segment")
+	}
+
+	segSize := int(binary.BigEndian.Uint16(p.source[pos:pos+2]))
+
+	table := make([]byte, segSize - 2)
+	copy(table, p.source[pos+2:pos+segSize])
+	p.sos = table
+
+	p.pos = pos+segSize
+	return nil
+}
+
+func(d *Parser) parseScan() error {
+	table := make([]byte, d.endPos - d.pos)
+	copy(table, d.source[d.pos:d.endPos])
+	d.scan = table
+	d.pos = d.endPos
+	return nil
+}
